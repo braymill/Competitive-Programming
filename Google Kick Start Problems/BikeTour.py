e923bbc0a11677b61988d9ede8ea865126d9a105
Brayden Miller <braydenmiller33@gmail.com>
2020-05-16 03:39:12 -0500

    updated bike tour

--- Google Kick Start Problems/BikeTour.py
+++ Google Kick Start Problems/BikeTour.py
@@ -4,6 +4,6 @@ for i in range(1, cases + 1):
     l = list(map(int, input().split()))
     c = 0
     for j in range(1, len(l) - 1):
-        if (l[j] > l[j-1] and l[j] > l[j+1]):
+        if l[j] > l[j-1] and l[j] > l[j+1]:
             c += 1
     print(f'Case #{i}: {c}')
