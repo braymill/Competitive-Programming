292fa5ff8a7d4055fc37881ff96e7f2547518dac
Brayden Miller <braydenmiller33@gmail.com>
2020-05-16 01:17:55 -0500

    added python files

--- Codeforces Problems/405A/GravityFlip.py
+++ Codeforces Problems/405A/GravityFlip.py
@@ -1,16 +1,5 @@
-#python 2 implementation
-
-def main():
-    cases = int(input())
-    list = []
-    line = str(raw_input())
-    idx = 0
-    list = line.split(' ')
-    newlist = []
-    for s in list:
-        newlist.append(int(s))
-    newlist = sorted(newlist)
-    for i in newlist:
-        print(str(i)),
-        
-main() 
+l = int(input())
+li = list(map(int, input().split()))
+li = sorted(li)
+for i in li:
+    print(f'{i}', end=' ')
---
+++ Google Kick Start Problems/BikeTour.py
@@ -0,0 +1,9 @@
+cases = int(input())
+for i in range(1, cases + 1):
+    leng = input()
+    l = list(map(int, input().split()))
+    c = 0
+    for j in range(1, len(l) - 1):
+        if (l[j] > l[j-1] and l[j] > l[j+1]):
+            c += 1
+    print('Case #{i}: {c}'.format(**locals()))
