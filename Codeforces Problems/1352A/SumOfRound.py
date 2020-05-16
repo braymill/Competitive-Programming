519d5ebf9ff93c15245f685617dad6f3cd7c9fe4
Brayden Miller <braydenmiller33@gmail.com>
2020-05-15 20:02:16 -0500

    added sumofround python implementation

---
+++ Codeforces Problems/1352A/SumOfRound.py
@@ -0,0 +1,33 @@
+# python implementation - uses less memory and is faster
+
+def main():
+    cases = input()
+    for i in range(cases):
+        curr = input()
+        if (curr > 0):
+            places = [False, False, False, False]
+            s = str(curr)
+            count = 0
+            a = len(s) - 1
+            while (a >= 0):
+                if (s[a] != '0'):
+                    places[a] = True
+                    count += 1
+                a -= 1
+            if (i == 0):
+                print('\n')
+            if (count <= 1):
+                print(1)
+                print(s)
+            else:
+                print(count)
+                a = len(s) - 1
+                toprint = ""
+                while (a >= 0):
+                    if (places[a]):
+                        digit = int(s[a])
+                        if (digit != 0):
+                            toprint += str(digit * (10 ** (len(s) - a - 1))) + " "
+                    a -= 1
+                print(toprint)
+main()
