59f1402c3e15b36b2f7db4366a6fd3e8dbf0f823
Brayden Miller <braydenmiller33@gmail.com>
2020-05-26 03:59:06 -0500

    fixed formatting

--- Codeforces Problems/1335A/candy.cpp
+++ Codeforces Problems/1335A/candy.cpp
@@ -17,7 +17,7 @@ int main() {
 				cout << n / 2 - 1 << "\n";
 			}
 			else {
-				long long a = (long long)floor(n / 2);
+				long long a = (long long) floor(n / 2);
 				cout << a << "\n";
 			}
 		}
