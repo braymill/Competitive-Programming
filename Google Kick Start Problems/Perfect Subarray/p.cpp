aba39e4191e3f16e1c4becd278eb1943aea1207b
Brayden Miller <braydenmiller33@gmail.com>
2020-05-25 23:38:46 -0500

    perfect subarray

---
+++ Google Kick Start Problems/Perfect Subarray/p.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    for (int i = 1; i < T + 1; i++){
+        int n;
+        cin >> n;
+        long a[100005];
+        int total = 0;
+        double num;
+        double sum;
+        for (int j = 0; j < n; j++){
+            sum = a[j];
+            for (int k = j; k < n; k++){
+                if (k != j){
+                    sum += a[k]
+                }
+                num = sqrt(sum);
+                if ((int) num == num){
+                    total++;
+                }
+            }
+        }
+        string out = "Case #" + to_string(i) + ": " + to_string(total);
+    }
+
+}
