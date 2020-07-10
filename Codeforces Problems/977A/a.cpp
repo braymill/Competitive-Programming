40c93e8fc939ce08ac495eaac1ca6e7d03a5feca
Brayden Miller <braydenmiller33@gmail.com>
2020-07-09 19:27:39 -0500

    added 977A

---
+++ Codeforces Problems/977A/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n, k;
+    cin >> n >> k;
+    while (k--){
+        if (n % 10 == 0){
+            n /= 10;
+        } else {
+            n--;
+        }
+    }
+    cout << n;
+}
